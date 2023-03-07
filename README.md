e286748acc435c8b2f06ab3701c92a8c9045880b
Tom Keller <kellertk@amazon.com>
2023-03-07 11:04:23 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# ts-boilerplate
- Template for a TS project with linting
+So, FreePBX ships with Node 8, which is insanely out of date and not even
+supported by the AWS JS SDK anymore. The built in way to use the Polly TTS
+engine uses very old versions of JS packages to get done what it needs to do and
+I think that's silly.
 
-Change me.
+So this is a rewrite in modern TypeScript that's compiled to a single file that
+can run on Node 8.
---
+++ src/polly.ts
@@ -0,0 +1,50 @@
+import { load } from 'ts-dotenv';
+const env = load({
+  AWS_ACCESS_KEY_ID: String,
+  AWS_SECRET_ACCESS_KEY: String,
+  AWS_REGION: String,
+});
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+const argv = yargs(hideBin(process.argv)).options({
+  mp3: { type: 'string', demandOption: true },
+  text: { type: 'string', demandOption: true },
+  wav: { type: 'string', demandOption: true },
+});
+import { PollyClient, SynthesizeSpeechCommand } from '@aws-sdk/client-polly';
+import { writeFile } from 'fs/promises';
+import { execSync } from 'child_process';
+
+const main = async () => {
+  const { mp3, text, wav } = await argv.argv;
+  const response = await new PollyClient({
+    region: env.AWS_REGION,
+    credentials: {
+      accessKeyId: env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    },
+  }).send(
+    new SynthesizeSpeechCommand({
+      OutputFormat: 'mp3',
+      Text: text,
+      TextType: 'text',
+      VoiceId: 'Ruth',
+      Engine: 'neural',
+      SampleRate: '8000',
+    })
+  );
+  if (response.AudioStream) {
+    const array = await response.AudioStream.transformToByteArray();
+    await writeFile(mp3, array);
+    execSync(`lame --decode ${mp3} -b 8000 ${wav}`);
+    execSync(`rm -f ${mp3}`);
+  }
+};
+
+(async () => {
+  await main();
+})().catch(e => {
+  console.error(`Error: ${String(e)}`);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
